88a84365809e4f2659dcb6ea6dd56a1bec3073cc
Sanjanaa S <ssanjanaa2007@gmail.com>
2026-05-31 16:52:17 +0530

    Fix Render port issue

--- server/server.js
+++ server/server.js
@@ -92,6 +92,8 @@ app.put("/blogs/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
